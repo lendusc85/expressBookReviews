081d8d95bf2c15405733f8916b8277375b2cae73
Lendus Casey <lcasey@mauijim.com>
2024-09-13 22:15:41 -0500

    initial changes

--- final_project/index.js
+++ final_project/index.js
@@ -12,11 +12,23 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+    if(req.session.authentication){
+        let accessToken = req. session.authentication['accessToken'];
+        jwt.verify(accessToken, "access", (err, user) => {
+            if(!err){
+                req.user = user;
+                next();
+            } else{
+                return res.status(403).json({message: "User not authenticated"})
+            }
+        });
+    }
 });
  
 const PORT =5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
+app.use("/async", genl_AsyncRoutes);
 
 app.listen(PORT,()=>console.log("Server is running"));
